8205749b2320526e3ba11666ec12a978e1326242
dutchman1999 <160536806+dutchman1999@users.noreply.github.com>
2024-03-08 22:22:49 +0530

    Handle form using useState
    Save form data into state.

--- src/App.js
+++ src/App.js
@@ -6,29 +6,35 @@ import Cities from './components/city';
 import Employee from './components/employee';
 import Customer from './components/customer';
 import Items from './components/item';
-import {HashRouter, Route, Routes, Link } from 'react-router-dom';
-
+import { HashRouter, Route, Routes, Link } from 'react-router-dom';
+import Hook from './components/Hook';
+import Hook2 from './components/Hook2';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HashRouter className = "Container">
-            <header>
-              <Link to="/"> Company </Link>
-              <Link to="/Cities"> Cities </Link>
-              <Link to="/Employee"> Employee </Link>
-              <Link to="/Customer"> Customer </Link>
-              <Link to="/Items"> Items </Link>
-            </header>
-            <Routes>
-              <Route exact path='/' element = {  <Company/> }  > </Route>
-              <Route exact path='/Cities' element= { <Cities/> }>  </Route>
-              <Route exact path='/Employee' element = {<Employee/>}>  </Route>
-              <Route exact path='/Customer' element = { <Customer/> }>  </Route>
-              <Route exact path='/Items' element = { <Items/> } > </Route>
-            </Routes>
+        <HashRouter className="Container">
+          <header>
+            <Link to="/"> Company </Link>
+            <Link to="/Cities"> Cities </Link>
+            <Link to="/Employee"> Employee </Link>
+            <Link to="/Customer"> Customer </Link>
+            <Link to="/Items"> Items </Link>
+            <Link to="/Hook">Hook</Link>
+            <Link to="/FormFillup">Form Fill-up</Link>
+            <img align= 'left' src="https://blog.tubikstudio.com/wp-content/uploads/2016/10/horsy_animation_logo_tubik_studio-1.gif" alt="bug" height={100} />
+          </header>
+          <Routes>
+            <Route exact path='/' element={<Company />}  > </Route>
+            <Route exact path='/Cities' element={<Cities />}>  </Route>
+            <Route exact path='/Employee' element={<Employee />}>  </Route>
+            <Route exact path='/Customer' element={<Customer />}>  </Route>
+            <Route exact path='/Items' element={<Items />} > </Route>
+            <Route exact path='/Hook' element={<Hook />} ></Route>
+            <Route exact path='/FormFillup' element={<Hook2/>}></Route>
+          </Routes>
         </HashRouter>
-      
+
 
       </header>
     </div>
---
+++ src/components/Hook2.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+const Hook2 = () => {
+    let [newAdmission, setNewAdmission] = useState([]);
+    let [name, setName] = useState('');
+    let [Class, setClass] = useState('');
+    let [sub, setSub] = useState('');
+    let [fees, setFees] = useState('');
+
+
+    const saveFormData = () => {
+        let inputFields = { name: name, Class: Class, sub: sub, fees: fees };
+        setNewAdmission(inputFields = [...newAdmission, inputFields]);
+        setName('');
+        setClass('');
+        setSub('');
+        setFees("");
+    }
+
+    return (
+        <div>
+            <h1> Student Form Fill-up </h1>
+            <h1> Newly admission students are : Total: {newAdmission.length} </h1>
+            {
+
+
+                        <div>
+                            <table align="right">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Name</th>
+                                        <th>Class</th>
+                                        <th>Subjects</th>
+                                        <th>Fees</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                               { newAdmission.map((studentDetails, index) => { return(
+                                    <tr key={index}>
+                                        <td>{index}</td>
+                                        <td>{studentDetails.name}</td>
+                                        <td>{studentDetails.Class}</td>
+                                        <td>{studentDetails.sub}</td>
+                                        <td>{studentDetails.fees}</td>
+                                    </tr>
+                                )})}
+                                </tbody>
+                            </table>
+
+
+                        </div>
+                    
+
+            }
+
+            <table align="left">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <input type="text" onChange={(e) => { setName(e.target.value) }} value={name}></input>
+                    </tr>
+                    <tr>
+                        <th>Class</th>
+                        <input type="text" onChange={(e) => { setClass(e.target.value) }} value={Class} ></input>
+                    </tr>
+                    <tr>
+                        <th>Subjects</th>
+                        <input type="text" onChange={(e) => { setSub(e.target.value) }} value={sub} ></input>
+                    </tr>
+                    <tr>
+                        <th>Fees</th>
+                        <input type="text" onChange={(e) => { setFees(e.target.value) }} value={fees} ></input>
+                    </tr>
+                    <tr>
+                        <th colSpan={2} className="saveButton">
+                            <button  align="center" onClick={saveFormData}><b>Save</b> </button>
+                        </th>
+                    </tr>
+                </thead>
+
+            </table>
+        </div>
+    )
+}
+export default Hook2;
